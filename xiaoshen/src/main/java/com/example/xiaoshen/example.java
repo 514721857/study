36e60a86afc07ea4bcdf3dfea9a15483815132e5
514721857 <2090161812@qq.com>
2016-06-18 15:54:56 +0800

    Initial commit

---
+++ xiaoshen/src/main/java/com/example/xiaoshen/example.java
@@ -0,0 +1,15 @@
+package com.example.xiaoshen;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/6/16 0016.
+ */
+public class example {
+    public static void setting(){
+        System.out.println("test");
+
+    }
+
+
+}
